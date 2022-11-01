664c9d28dab9a2ffe8213079665aa1c4f0fe7024
paladinze <paladinze@hotmail.com>
2022-10-31 22:44:05 -0400

    feat(cg-r3f-starter): add shadow

--- apps/cg/r3f-starter/src/app/app.tsx
+++ apps/cg/r3f-starter/src/app/app.tsx
@@ -2,6 +2,13 @@ import { Canvas } from '@react-three/fiber';
 import Sketch from './sketch';
 import { ACESFilmicToneMapping, CineonToneMapping, sRGBEncoding } from 'three';
 import { Leva } from 'leva';
+import { RootState } from '@react-three/fiber/dist/declarations/src/core/store';
+
+const onCreateHandler = (state: RootState) => {
+  const { gl, scene } = state;
+  gl.setClearColor('#fff8dc', 1);
+  // scene.background = new THREE.Color('#ff0000')
+}
 
 export function App() {
   return (
@@ -9,6 +16,8 @@ export function App() {
       <Leva collapsed={false} />
       <div id='canvas-container'>
         <Canvas
+          shadows
+          // shadows
           dpr={[1, 2]} // dpr range
           gl={{
             antialias: true,
@@ -20,7 +29,9 @@ export function App() {
             near: 0.1,
             far: 200,
             position: [0, 5, 20]
-          }}>
+          }}
+          onCreated={onCreateHandler}
+        >
           <Sketch />
         </Canvas>
       </div>
--- apps/cg/r3f-starter/src/app/sketch.tsx
+++ apps/cg/r3f-starter/src/app/sketch.tsx
@@ -1,17 +1,32 @@
-import { DoubleSide, Group, Mesh } from 'three';
+import { DirectionalLight, DirectionalLightHelper, DoubleSide, Group, Mesh } from 'three';
 import { useFrame, useThree } from '@react-three/fiber';
 import { useRef } from 'react';
 import CustomMesh from './custom-mesh';
 import { button, useControls } from 'leva';
-import { Float, Html, MeshReflectorMaterial, OrbitControls, PivotControls, Text } from '@react-three/drei';
+import { Float, Html, OrbitControls, PivotControls, softShadows, Text, useHelper } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
+// PCSS soft shadow: heavy impact on performance
+softShadows({
+  frustum: 3.75,
+  size: 0.015,
+  near: 9.5,
+  samples: 17,
+  rings: 11
+});
+
 export default function Sketch() {
+  // meshes
   const cubeRef = useRef<Mesh>(null!);
   const sphereRef = useRef<Mesh>(null!);
   const planeRef = useRef<Mesh>(null!);
   const groundRef = useRef<Mesh>(null!);
   const groupRef = useRef<Group>(null!);
+
+  // helpers
+  const directionalLightRef = useRef<DirectionalLight>(null!);
+  useHelper(directionalLightRef, DirectionalLightHelper);
+
   const debugControls = useControls('scene', {
     perfPanelVisible: true,
     titlePos: {
@@ -23,7 +38,7 @@ export default function Sketch() {
     cubePos: {
       value: {
         x: 0,
-        y: 0,
+        y: 0
       },
       step: 0.01,
       joystick: 'invertY'
@@ -38,11 +53,11 @@ export default function Sketch() {
     cubePos: {
       value: {
         x: 0,
-        y: 0,
+        y: 0
       },
       step: 0.01,
       joystick: 'invertY'
-    },
+    }
   });
 
   const { camera, gl } = useThree();
@@ -63,8 +78,22 @@ export default function Sketch() {
   return <>
     {debugControls.perfPanelVisible && <Perf position='top-left' />}
     <ambientLight intensity={0.1} />
-    <directionalLight color='blue' position={[3, 0, 5]} />
+    <directionalLight
+      ref={directionalLightRef}
+      color='pink' position={[-3, 5, 5]}
+      castShadow={true}
+      shadow-mapSize={[1024, 1024]}
+      shadow-camera-near={1}
+      shadow-camera-far={10}
+      shadow-camera-top={2}
+      shadow-camera-right={2}
+      shadow-camera-bottom={-2}
+      shadow-camera-left={-2}
+    />
     <OrbitControls enableDamping={true} makeDefault />
+    {/*
+    <BakeShadows /> // render shadow only once
+    */}
     <Text
       font='assets/fonts/Bangers-Regular.ttf'
       color='salmon' fontSize={2} position={[0, debugControls.titlePos, 3]}
@@ -80,7 +109,7 @@ export default function Sketch() {
       <Float speed={5} floatIntensity={1.5}>
         <CustomMesh position-y={4} />
       </Float>
-      <mesh ref={cubeRef} position={[cubeDebugControls.cubePos.x, cubeDebugControls.cubePos.y, 0]}>
+      <mesh ref={cubeRef} position={[cubeDebugControls.cubePos.x, cubeDebugControls.cubePos.y, 0]} castShadow={true}>
         <boxGeometry />
         <meshStandardMaterial />
       </mesh>
@@ -103,15 +132,18 @@ export default function Sketch() {
         <planeGeometry />
         <meshBasicMaterial color={'red'} side={DoubleSide} />
       </mesh>
-      <mesh ref={groundRef} position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh ref={groundRef} position-y={-1} rotation-x={-Math.PI * 0.5} scale={10} receiveShadow={true}>
         <planeGeometry />
-        <MeshReflectorMaterial
-          mirror={0.5}
-          resolution={512}
-          blur={[1000, 1000]}
-          color='lightgrey'
-          mixBlur={1} />
+        {/*<MeshReflectorMaterial*/}
+        {/*  mirror={0.5}*/}
+        {/*  resolution={512}*/}
+        {/*  blur={[1000, 1000]}*/}
+        {/*  color='lightgrey'*/}
+        {/*  mixBlur={1}*/}
+        {/*  side={DoubleSide}*/}
+        {/*/>*/}
         {/*<meshBasicMaterial color='greenyellow' side={DoubleSide} />*/}
+        <meshStandardMaterial />
       </mesh>
     </group>
   </>;
