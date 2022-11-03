16dd48c2990739cdec43197e58c88d7890ac8c33
paladinze <paladinze@hotmail.com>
2022-11-03 19:18:53 -0400

    refactor(cg-zefolio): add wizard model

--- apps/cg/zefolio/src/app/components/wizard-model.tsx
+++ apps/cg/zefolio/src/app/components/wizard-model.tsx
@@ -1,6 +1,33 @@
-import { useGLTF } from '@react-three/drei';
+import { useAnimations, useGLTF } from '@react-three/drei';
+import { useEffect } from 'react';
+import * as THREE from 'three';
+
+/**
+ * Animations
+ *  PortalOpen
+ *  Still
+ *  Waiting
+ */
+enum Clips {
+  STILL = 'Still',
+  PORTAL_OPEN = 'PortalOpen',
+  WAITING = 'Waiting',
+}
 
 export default function WizardModel(props: any) {
-  const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/druid/model.gltf')
-  return <primitive object={scene} {...props} />
+  const model = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/druid/model.gltf');
+  const animations = useAnimations(model.animations, model.scene);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const magicAction = animations.actions[Clips.PORTAL_OPEN]!;
+      magicAction.setLoop(THREE.LoopOnce, 1);
+      magicAction.clampWhenFinished = true;
+      magicAction.enabled = true;
+      magicAction.play();
+    }, 3000);
+
+  }, []);
+
+  return <primitive object={model.scene} {...props} />;
 }
